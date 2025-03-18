287772e51bc1cf6c39622e654969fb5d68995276
mysteryharvest <109390561+mysteryharvest@users.noreply.github.com>
2025-03-18 15:46:30 -0700

    Update magicianCards.ts

--- src/data/cards/magicianCards.ts
+++ src/data/cards/magicianCards.ts
@@ -8,7 +8,7 @@ export const magicianCards: OracleCard[] = [
     name: 'Yan', 
     type: 'magician', 
     category: 'Magician',
-    description: 'The Temperate. Yan, the First Magician, is kind and mild. Yan deescalates bad situations, and provides sweetness and contentment.',
+    description: 'The Temperate. Yan, the First Magician, is kind and mild. Yan deescalates bad situations, and provides sweetness and contentment. Yan also brings things that become richer and more valuable with age.',
     imageUrl: '/images/oracleCards/magician-yan-card.png',
     attributes: ['Grape', 'Blackberry']
   },
@@ -17,7 +17,7 @@ export const magicianCards: OracleCard[] = [
     name: 'Tan', 
     type: 'magician', 
     category: 'Magician',
-    description: 'The Kind. Tan means kindness both received and delivered.',
+    description: 'The Kind. Tan means kindness both received and delivered. Tan appreciates nothing more than small acts of kindness. However, Tan also teaches us that there is a difference between being "kind" and being "nice." Sometimes kindness can hurt.',
     imageUrl: '/images/oracleCards/magician-tan-card.png',
     attributes: ['Honeysuckle', 'Nettle']
   },
@@ -26,7 +26,7 @@ export const magicianCards: OracleCard[] = [
     name: 'Tether', 
     type: 'magician', 
     category: 'Magician',
-    description: 'The Calm. Tether provides the kind of simple calmness one experiences just before falling asleep.',
+    description: 'The Calm. Tether provides the kind of simple calmness one experiences just before falling asleep. No matter how difficult the day has been, Tether provides a quiet, peaceful respite.',
     imageUrl: '/images/oracleCards/magician-tether-card.png',
     attributes: ['Sweet Pea', 'Rose']
   },
@@ -35,7 +35,7 @@ export const magicianCards: OracleCard[] = [
     name: 'Mether', 
     type: 'magician', 
     category: 'Magician',
-    description: 'The Bountiful. Mether helps the querent recognize and benefit from all positive currents flowing in her direction.',
+    description: 'The Bountiful. Mether helps us recognize, and benefit from, all positive currents flowing in our direction. When everything is going wrong, Mether provides perspective, and shows us small opportunities for improvement.',
     imageUrl: '/images/oracleCards/magician-mether-card.png',
     attributes: ['Gourd', 'Locust']
   },
@@ -44,7 +44,7 @@ export const magicianCards: OracleCard[] = [
     name: 'Pip', 
     type: 'magician', 
     category: 'Magician',
-    description: 'The Joyous. Pip is the kind of "joy" one experiences when something particularly vexing has been overcome.',
+    description: 'The Joyous. Pip is the kind of "joy" one experiences when something particularly vexing has been overcome. A long period of difficulties comes to its conclusion; something stuck finally becomes dislodged.',
     imageUrl: '/images/oracleCards/magician-pip-card.png',
     attributes: ['Poison Ivy', 'Hawthorn']
   },
@@ -53,7 +53,7 @@ export const magicianCards: OracleCard[] = [
     name: 'Azer', 
     type: 'magician', 
     category: 'Magician',
-    description: 'The Hilarious. Azer is freaking hilarious. As opposed to joy, hilarity may be more fleeting, but the absolute release of sheer comedy is immensely helpful for the querent.',
+    description: 'The Hilarious. Azer is freaking hilarious. As opposed to joy, hilarity may be more fleeting, but the absolute release of sheer comedy is immensely helpful.',
     imageUrl: '/images/oracleCards/magician-azer-card.png',
     attributes: ['Wonga Wonga', 'Thistle']
   },
@@ -62,7 +62,7 @@ export const magicianCards: OracleCard[] = [
     name: 'Sezar', 
     type: 'magician', 
     category: 'Magician',
-    description: 'The Wonderful. Sezar defeats Monsters by disguising itself as a toxin, which can be neutralized if the querent knows how.',
+    description: 'The Wonderful. Sezar defeats Monsters by disguising itself as a toxin. This card suggests that to succeed, you may need to insinuate yourself into a situation, or find a way to innoculate yourself.',
     imageUrl: '/images/oracleCards/magician-sezar-card.png',
     attributes: ['Nightshade', 'Devil\'s Club']
   },
@@ -71,7 +71,7 @@ export const magicianCards: OracleCard[] = [
     name: 'Akker', 
     type: 'magician', 
     category: 'Magician',
-    description: 'The Giver. Akker gives gifts.',
+    description: 'The Giver. Akker gives gifts. Akker loves children. Akker has an enormous beard and visits houses, bringing presents and candies and all of the good things.',
     imageUrl: '/images/oracleCards/magician-akker-card.png',
     attributes: ['Holly', 'Ivy']
   }
