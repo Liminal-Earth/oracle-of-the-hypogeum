a6609e733032da0dde619c5e2c1f4407f40bdaad
mysteryharvest <109390561+mysteryharvest@users.noreply.github.com>
2025-03-19 09:15:34 -0700

    Update magicianCards.ts

--- src/data/cards/magicianCards.ts
+++ src/data/cards/magicianCards.ts
@@ -8,7 +8,7 @@ export const magicianCards: OracleCard[] = [
     name: 'Yan', 
     type: 'magician', 
     category: 'Magician',
-    description: 'The Temperate. Yan, the First Magician, is kind and mild. Yan deescalates bad situations, and provides sweetness and contentment. Grape is cultivated, as are Yan’s influences, but Blackberry, often invasive, represents Yan’s wilder side. Blackberries heal the scarred soil beneath, and grapes are sweet, refreshing, and delicious.',
+    description: 'Temperate. Yan, the First Magician, is kind and mild. Yan deescalates bad situations, and provides sweetness and contentment. Grape is cultivated, as are Yan’s influences, but Blackberry, often invasive, represents Yan’s wilder side. Blackberries heal the scarred soil beneath, and grapes are sweet, refreshing, and delicious.',
     imageUrl: '/images/oracleCards/magician-yan-card.png',
     attributes: ['Grape', 'Blackberry']
   },
@@ -17,7 +17,7 @@ export const magicianCards: OracleCard[] = [
     name: 'Tan', 
     type: 'magician', 
     category: 'Magician',
-    description: 'The Kind. Tan appreciates nothing more than small acts of kindness. However, Tan also teaches us that there is a difference between being "kind" and being "nice." Sometimes kindness can hurt. Tan means kindness both received and delivered. Honeysuckle cannot be eaten, but the scent and the flavor produced by its flowers represent the kind of kindness the querent will receive, or give. Nettle is a useful plant, but only after one learns how to safely use it.',
+    description: 'Kindness. Tan appreciates nothing more than small acts of kindness. However, Tan also teaches us that there is a difference between being "kind" and being "nice." Sometimes kindness can hurt. Tan means kindness both received and delivered. Honeysuckle cannot be eaten, but the scent and the flavor produced by its flowers represent the kind of kindness the querent will receive, or give. Nettle is a useful plant, but only after one learns how to safely use it.',
     imageUrl: '/images/oracleCards/magician-tan-card.png',
     attributes: ['Honeysuckle', 'Nettle']
   },
@@ -26,7 +26,7 @@ export const magicianCards: OracleCard[] = [
     name: 'Tether', 
     type: 'magician', 
     category: 'Magician',
-    description: 'The Calm. Tether provides the kind of simple calmness one experiences just before falling asleep. No matter how difficult the day has been, Tether provides a quiet, peaceful respite. Tether provides the kind of simple calmness one experiences just before falling asleep. This Magician is, perhaps, the most peaceful of the Magicians. Tether can represent happiness in love and relationships. As with Sweet Pea and Rose, even though the flowers wither/fall, the plants in question produce delightful and useful fruit.',
+    description: 'Calm. Tether provides the kind of simple calmness one experiences just before falling asleep. No matter how difficult the day has been, Tether provides a quiet, peaceful respite. Tether provides the kind of simple calmness one experiences just before falling asleep. This Magician is, perhaps, the most peaceful of the Magicians. Tether can represent happiness in love and relationships. As with Sweet Pea and Rose, even though the flowers wither/fall, the plants in question produce delightful and useful fruit.',
     imageUrl: '/images/oracleCards/magician-tether-card.png',
     attributes: ['Sweet Pea', 'Rose']
   },
@@ -35,7 +35,7 @@ export const magicianCards: OracleCard[] = [
     name: 'Mether', 
     type: 'magician', 
     category: 'Magician',
-    description: 'The Bountiful. Mether helps the querent recognize and benefit from all positive currents flowing in her direction. The Bounty in question might not be physical, and may be slowly accumulating, but it’s definitely happening. Like the gourd, Mether not only can be used in many ways, but when left in one’s compost, will produce fertile soil and sometimes additional gourds. And, like the locust, Mether increases the fertility of any soil into which it is planted. When everything is going wrong, Mether provides perspective, and shows us small opportunities for improvement.',
+    description: 'Bounty. Mether helps the querent recognize and benefit from all positive currents flowing in her direction. The Bounty in question might not be physical, and may be slowly accumulating, but it’s definitely happening. Like the gourd, Mether not only can be used in many ways, but when left in one’s compost, will produce fertile soil and sometimes additional gourds. And, like the locust, Mether increases the fertility of any soil into which it is planted. When everything is going wrong, Mether provides perspective, and shows us small opportunities for improvement.',
     imageUrl: '/images/oracleCards/magician-mether-card.png',
     attributes: ['Gourd', 'Locust']
   },
@@ -44,7 +44,7 @@ export const magicianCards: OracleCard[] = [
     name: 'Pip', 
     type: 'magician', 
     category: 'Magician',
-    description: 'The Joyous.What does Poison Ivy have to do with Joy? To the Quatrians, poison ivy represented joy when one finally stopped itching! Pip is the kind of “joy” one experiences when something particularly vexing has been overcome. Hawthorn, sacred to Anthuor, brings joy in both winter (when the berries appear, colorful and useful as a medicine for the heart), but also in spring when the white flowers snow upon children.',
+    description: 'Joy. What does Poison Ivy have to do with Joy? To the Quatrians, poison ivy represented joy when one finally stopped itching! Pip is the kind of “joy” one experiences when something particularly vexing has been overcome. Hawthorn, sacred to Anthuor, brings joy in both winter (when the berries appear, colorful and useful as a medicine for the heart), but also in spring when the white flowers snow upon children.',
     imageUrl: '/images/oracleCards/magician-pip-card.png',
     attributes: ['Poison Ivy', 'Hawthorn']
   },
@@ -53,7 +53,7 @@ export const magicianCards: OracleCard[] = [
     name: 'Azer', 
     type: 'magician', 
     category: 'Magician',
-    description: 'The Hilarious. Azer is freaking hilarious. As opposed to joy, hilarity may be more fleeting, but the absolute release of sheer comedy is immensely helpful for the querent. Wonga Wonga (Pandorea pandorana) isn’t inherently funny– the querent has to define what makes it funny. Thistle is dangerous, but in the proper hands, is delicious, and artichoke flavored.',
+    description: 'Hilarity. Azer is freaking hilarious. As opposed to joy, hilarity may be more fleeting, but the absolute release of sheer comedy is immensely helpful for the querent. Wonga Wonga (Pandorea pandorana) isn’t inherently funny– the querent has to define what makes it funny. Thistle is dangerous, but in the proper hands, is delicious, and artichoke flavored.',
     imageUrl: '/images/oracleCards/magician-azer-card.png',
     attributes: ['Wonga Wonga', 'Thistle']
   },
@@ -62,7 +62,7 @@ export const magicianCards: OracleCard[] = [
     name: 'Sezar', 
     type: 'magician', 
     category: 'Magician',
-    description: 'The Wonderful. Sezar defeats Monsters by disguising itself as a toxin, which can be neutralized if the querent knows how. Black Nightshade (Solanum nigrum), in Western culture, is a poison according to “common knowledge,” so toxic it’s destroyed and befouled as often as possible. However, Solanum nigrum is actually completely safe to eat when identified correctly. It’s made into pies, cooked as greens, and generally just fine. Devil’s Club, a horrifying collection of spikes and thorns, is a ginseng relative whose roots are powerful medicine. When initiated into Sezar’s mysteries, one cannot help but experience success.',
+    description: 'Wonder. Sezar defeats Monsters by disguising itself as a toxin, which can be neutralized if the querent knows how. Black Nightshade (Solanum nigrum), in Western culture, is a poison according to “common knowledge,” so toxic it’s destroyed and befouled as often as possible. However, Solanum nigrum is actually completely safe to eat when identified correctly. It’s made into pies, cooked as greens, and generally just fine. Devil’s Club, a horrifying collection of spikes and thorns, is a ginseng relative whose roots are powerful medicine. When initiated into Sezar’s mysteries, one cannot help but experience success.',
     imageUrl: '/images/oracleCards/magician-sezar-card.png',
     attributes: ['Nightshade', 'Devil\'s Club']
   },
@@ -71,7 +71,7 @@ export const magicianCards: OracleCard[] = [
     name: 'Akker', 
     type: 'magician', 
     category: 'Magician',
-    description: 'The Giver. Akker gives gifts. Holly and Ivy– both so prolific that in some places they’re considered invasive– have long been associated with protection against malevolent influences. Often, when looking at plants, one only considers the physical benefits: how can I eat this or how can this be used as medicine? Holly and Ivy, however, provide gifts which aren’t so obvious, as does Akker.',
+    description: 'Gifts. Akker gives gifts. Holly and Ivy– both so prolific that in some places they’re considered invasive– have long been associated with protection against malevolent influences. Often, when looking at plants, one only considers the physical benefits: how can I eat this or how can this be used as medicine? Holly and Ivy, however, provide gifts which aren’t so obvious, as does Akker.',
     imageUrl: '/images/oracleCards/magician-akker-card.png',
     attributes: ['Holly', 'Ivy']
   }
