418691193c4fa039bc7af6bd05056e7e9928c08b
mysteryharvest <109390561+mysteryharvest@users.noreply.github.com>
2025-03-20 15:40:57 -0700

    Update AboutSection.tsx

--- src/components/home/AboutSection.tsx
+++ src/components/home/AboutSection.tsx
@@ -12,11 +12,11 @@ const AboutSection: React.FC = () => {
             <p>
               The Oracle of the Hypogeum is a series of 48 symbols which originated in the Ancient Quatrian culture. The Quatrians used these symbols to help remythologize their landscapes, both internal and external. They were utilized for divination, but also as a symbolic alphabet which could be used to generate stories and mythic structures, rituals, and spells. 
             </p>
-            
+            <p></p>
             <p>
               Although specific traditions and customs varied from biome to biome, the ideas represented by these glyphs were universal. They could be found, individually or collectively, on everything from doorposts to road signs to tattoos. They were said to have been taught to humans by ANTHUOR, the Quatrian deer spirit, who communicated the symbols via shapes seen in the murmurations of starlings. The starlings took flight from within the Hypogeum, a mysterious stone chamber central to Quatrian myth.
             </p>
-            
+            <p></p>
             <p>
               As ubiquitous within Quatrian culture as corporate logos are in Late Capitalist neoliberalism, the primary use for the Symbols seems to have been a way to express meaning across a variety of cultural contexts. Just as someone living in Southeast Asia might recognize the "Golden Arches" as representative of McDonald's, so a Quatrian from a sub-polar settlement would understand that the symbol for "Life Force" depicted a plant emerging from a seed.
             </p>
@@ -26,11 +26,11 @@ const AboutSection: React.FC = () => {
             <p>
               Every society practiced some form of what we now refer to as "Divination," and the Quatrians were no exception. That said, our understanding of divination (via Tarot Cards, the I Ching, etc.) depends on a much different conception of time than that of the various societies of Quatria.
             </p>
-            
+            <p></p>
             <p>
               Quatrians conceived of experience as a kind of "bubbling up" of instances from an underlying Ground of Being. Instead of our linear timeline, or even something cyclical, "time," in Quatria, resembled the surface of a lake, and "instances" as susurrations on the face of the water.
             </p>
-            
+            <p></p>
             <p>
               Quatrian "divination" seems to have been used to identify other factors causing the entire singularity at which the reader found herself. Thus, the diviner uses the cards to quite literally make a map, of consciousness or otherwise.
             </p>
